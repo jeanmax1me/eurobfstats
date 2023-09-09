749136ee8e5137a8de873c4a3ec7d339b14e5ac0
jeanmax1me <jeanmaximegilbert@gmail.com>
2023-09-09 22:55:58 +0200

    height solved pog

--- app/components/Height/page.jsx
+++ app/components/Height/page.jsx
@@ -1,28 +1,44 @@
-import React from "react";
-import {RadioGroup, Radio} from "@nextui-org/react";
+import React, { useState, useEffect } from "react";
+import { RadioGroup, Radio } from "@nextui-org/react";
 import useSharedState from "../useSharedState";
 
-export default function App() {
-    const {minHeight, setMinHeight}  = useSharedState();
+export default function Height() {
+  const { minHeight, setMinHeight } = useSharedState();
+  const availableHeights = ["160", "165", "170", "175", "180", "185", "190"];
+  const selectedIndex = availableHeights.indexOf(minHeight);
+  const [selectedHeightIndex, setSelectedHeightIndex] = useState(selectedIndex);
 
-    const handleMinHeightChange = (value) => {
-        setMinHeight(value);
-      };
+  useEffect(() => {
+    setSelectedHeightIndex(selectedIndex);
+  }, [minHeight, selectedIndex]);
+
+  const handleMinHeightChange = (index) => {
+    const selectedHeight = availableHeights[index];
+    setSelectedHeightIndex(index);
+    setMinHeight(selectedHeight);
+    console.log("test", selectedHeight, minHeight);
+  };
+
+  useEffect(() => {
+    console.log("minHeight changed:", minHeight);
+  }, [minHeight]);
 
   return (
     <RadioGroup
       label="Select his minimum height"
       orientation="horizontal"
       className="custom-radio-group bg-[#E7F1FE] rounded-2xl p-3"
-      onChange={handleMinHeightChange}
     >
-      <Radio value="160">160cm</Radio>
-      <Radio value="165">165cm</Radio>
-      <Radio value="170">170cm</Radio>
-      <Radio value="175">175cm</Radio>
-      <Radio value="180">180cm</Radio>
-      <Radio value="185">185cm</Radio>
-      <Radio value="190">190cm</Radio>
+      {availableHeights.map((height, index) => (
+        <Radio
+          key={height}
+          value={height}
+          data-selected={index === selectedHeightIndex}
+          onChange={() => handleMinHeightChange(index)}
+        >
+          {height}cm
+        </Radio>
+      ))}
     </RadioGroup>
   );
 }
